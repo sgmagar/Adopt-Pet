8634cc30e3461097b3b612b3e794e5ee9d62d6ce
Saurav <sp.gharti@gmail.com>
2020-11-10 13:09:26 +0545

    add custom babel presets and eslint and use state attr on details page

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,40 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
 
-const Details = (props) => {
-  return (
-    <pre>
-      <codd>{JSON.stringify(props, null, 4)}</codd>
-    </pre>
-  );
-};
+class Details extends React.Component {
+  state = {
+    loading: true,
+  };
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.tate}`,
+        description: animal.description,
+        breed: animal.breeds.primary,
+        media: animal.photos,
+        loading: false,
+      });
+    }, console.error);
+  }
+  render() {
+    if (this.state.loading) {
+      return <h1>loading...</h1>;
+    }
+    const { animal, breed, location, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
